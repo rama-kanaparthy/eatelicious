a0128b7492a357ebb68863e9ed16c06f06aea203
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2025-03-10 23:32:35 +0530

    positioning text widget in center

--- app/src/main/java/com/rama/eatelicious/MainActivity.kt
+++ app/src/main/java/com/rama/eatelicious/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -41,15 +43,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     val isDarkTheme = isSystemInDarkTheme()
-    Text(
-        text = "Hello $name!",
-        color = if(isDarkTheme) Purple80 else Pink40,
-        fontSize = 30.sp,
-        fontWeight = FontWeight.Bold,
-        fontFamily = FontFamily.Monospace,
-        style = MaterialTheme.typography.bodyLarge,
-        modifier = modifier.padding(32.dp)
-    )
+    Box(modifier=modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Text(
+            text = "Hello $name!",
+            color = if(isDarkTheme) Purple80 else Pink40,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = modifier.padding(32.dp)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
