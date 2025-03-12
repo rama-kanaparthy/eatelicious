3fb3e2f14588a33e7c69205d9cbec9df3f774fb9
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2025-03-12 11:15:21 +0530

    UI

--- app/src/main/java/com/rama/eatelicious/MainActivity.kt
+++ app/src/main/java/com/rama/eatelicious/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rama.eatelicious
 
+import android.content.res.Configuration
 import android.icu.number.Scale
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,28 +12,46 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rama.eatelicious.Restaurant
@@ -41,22 +60,6 @@ import com.rama.eatelicious.ui.theme.Pink40
 import com.rama.eatelicious.ui.theme.Purple80
 
 data class Restaurant(val name: String, val tagLine: String, val img: Int)
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EateliciousTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    RestaurantList(
-                        restaurantList
-                    )
-                }
-            }
-        }
-    }
-}
 
 val restaurantList = listOf<Restaurant>(
     Restaurant("Aaradya1","feel home", R.drawable.restaurant_01),
@@ -71,11 +74,74 @@ val restaurantList = listOf<Restaurant>(
     Restaurant("Aaradya10","feel home", R.drawable.restaurant_10)
 )
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EateliciousTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    RestaurantApp()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RestaurantList(restaurants: List<Restaurant>){
+fun RestaurantApp(){
+    val restaurants = remember {
+        mutableStateListOf(
+            Restaurant("Aaradya1","feel home", R.drawable.restaurant_01),
+            Restaurant("Aaradya2","feel home", R.drawable.restaurant_02),
+            Restaurant("Aaradya3","feel home", R.drawable.restaurant_03),
+            Restaurant("Aaradya4","feel home", R.drawable.restaurant_04),
+            Restaurant("Aaradya5","feel home", R.drawable.restaurant_05),
+            Restaurant("Aaradya6","feel home", R.drawable.restaurant_06),
+            Restaurant("Aaradya7","feel home", R.drawable.restaurant_07),
+            Restaurant("Aaradya8","feel home", R.drawable.restaurant_08),
+            Restaurant("Aaradya9","feel home", R.drawable.restaurant_09),
+            Restaurant("Aaradya10","feel home", R.drawable.restaurant_10)
+        )
+    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Eatelicious",
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    restaurants.add(restaurants.random())
+                }
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = null)
+            }
+        }
+    ) { innerPadding ->
+        RestaurantList(
+            innerPadding, restaurants
+        )
+    }
+}
+
+@Composable
+fun RestaurantList(contentPadding: PaddingValues, restaurants: List<Restaurant>){
 
     Column(
-        verticalArrangement = Arrangement.SpaceAround
+        verticalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier.padding(contentPadding)
+            .verticalScroll(rememberScrollState())
     ) {
         for (restaurant in restaurants){
             RestaurantCard(restaurant)
@@ -86,11 +152,17 @@ fun RestaurantList(restaurants: List<Restaurant>){
 
 @Composable
 fun RestaurantCard(restaurant: Restaurant){
-    Surface(
-        shape = MaterialTheme.shapes.medium,
-        shadowElevation = 4.dp,
-        color = Color.White,
-        modifier = Modifier.fillMaxWidth()
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = CutCornerShape(
+            20.dp
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
+        colors = CardDefaults.cardColors(
+            Color.White
+        )
     ) {
         Row(modifier = Modifier.padding(8.dp)) {
            Image(painter = painterResource(restaurant.img),
@@ -128,16 +200,19 @@ fun RestaurantCard(restaurant: Restaurant){
 
 @Preview(showBackground = true, name = "restaurantListPreview", showSystemUi = true,
     device = "id:pixel_9_pro", backgroundColor = 0xFF551CB0,
-    wallpaper = androidx.compose.ui.tooling.preview.Wallpapers.RED_DOMINATED_EXAMPLE,
+    wallpaper = Wallpapers.RED_DOMINATED_EXAMPLE,
     fontScale = 1.0f,
-    uiMode = android.content.res.Configuration.UI_MODE_NIGHT_NO or android.content.res.Configuration.UI_MODE_TYPE_UNDEFINED
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_UNDEFINED
 )
+
 @Composable
 fun RestaurantListPreview() {
     EateliciousTheme {
-        RestaurantList(
-            restaurantList
-        )
+        Scaffold { innerPadding ->
+            RestaurantList(
+                innerPadding, restaurantList
+            )
+        }
     }
 }
 
