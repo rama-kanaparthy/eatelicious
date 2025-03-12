077de957fe3d8baf80f9eba71959642b2de67e98
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2025-03-12 13:22:15 +0530

    UI

--- app/src/main/java/com/rama/eatelicious/MainActivity.kt
+++ app/src/main/java/com/rama/eatelicious/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.rama.eatelicious
 
 import android.content.res.Configuration
-import android.icu.number.Scale
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,12 +24,12 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -39,24 +37,20 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.rama.eatelicious.Restaurant
 import com.rama.eatelicious.ui.theme.EateliciousTheme
-import com.rama.eatelicious.ui.theme.Pink40
 import com.rama.eatelicious.ui.theme.Purple80
 
 data class Restaurant(val name: String, val tagLine: String, val img: Int)
@@ -81,11 +75,78 @@ class MainActivity : ComponentActivity() {
         setContent {
             EateliciousTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    RestaurantApp()
+                    ProfileRender()
                 }
             }
         }
     }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun ProfileRender(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Eatelicious",
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+
+    ) { innerPadding ->
+        RestaurantProfile(
+            innerPadding
+        )
+    }
+}
+
+@Composable
+fun RestaurantProfile(contentPadding: PaddingValues){
+
+    Card (
+        modifier = Modifier.padding(contentPadding).fillMaxWidth(),
+        shape = RectangleShape,
+        colors = CardDefaults.cardColors(
+            MaterialTheme.colorScheme.background
+        )
+    ) {
+
+        var name by remember {
+            mutableStateOf("")
+        }
+        Box(
+            modifier = Modifier.padding(16.dp).fillMaxSize()
+        ){
+            Box(Modifier.fillMaxSize()){
+                Column {
+                    Box(modifier = Modifier.fillMaxWidth()){
+                        Text(
+                            text = "Restaurant "
+                        )
+                    }
+                    OutlinedTextField(
+                        value = name,
+                        onValueChange = {
+                            name = it
+                        },
+                        label = {
+                            Text("Enter Text")
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+        }
+    }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
